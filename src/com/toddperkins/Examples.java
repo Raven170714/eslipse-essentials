4f1e3ada37cd99d8256d9be8b246bb2e19fcb548
Emma Davison <24436348@edgehill.ac.uk>
2021-09-27 11:21:18 +0100

    commit and push!

--- src/com/toddperkins/Examples.java
+++ src/com/toddperkins/Examples.java
@@ -3,11 +3,11 @@ package com.toddperkins;
 public class Examples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		// TODO Auto-generated method stub
 
 	}
 
 	public void example() {
-
+		String str = "";
 	}
 }
